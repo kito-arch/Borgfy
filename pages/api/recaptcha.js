e5a2740b692def68abe2f9af11f1ce3697782d32
Husnain <h.m.khan315@gmail.com>
2022-12-20 00:59:34 +0500

    Changes

--- pages/api/recaptcha.js
+++ pages/api/recaptcha.js
@@ -2,7 +2,7 @@
 
 export default async function handler(req, res){
     const resp = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.NEXT_PUBLIC_SECRET}&response=${req.body.token}`, {method: 'POST'});
-    console.log(process.env.NEXT_PUBLIC_SECRET)
+    // console.log(process.env.NEXT_PUBLIC_SECRET)
     // console.log(resp)
     const data = await resp.json();
     // console.log(data)
